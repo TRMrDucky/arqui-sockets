00eaad9e2992f97425112b8c94d378c4c37c6edd
TRMrDucky <vv094673@gmail.com>
2025-10-29 19:53:34 -0700

    iniciado el proyecto

---
+++ AS-Emisor/src/main/java/com/mycompany/as/client/ASClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.as.client;
+
+/**
+ *
+ * @author 52644
+ */
+public class ASClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AS-Receptor/src/main/java/com/mycompany/as/receptor/ASReceptor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.as.receptor;
+
+/**
+ *
+ * @author 52644
+ */
+public class ASReceptor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
